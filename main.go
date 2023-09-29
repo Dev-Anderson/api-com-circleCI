0c07575b5b8ae6d5fcfe52e51754d7367f6c1b7f
Anderson da Silva <adersoosilvaa@gmail.com>
2023-09-29 13:12:00 -0300

    :zap: Teste de circleCI

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ var companies = []Company{
 	{ID: "1", Name: "Dell", CEO: "Michael Dell", Revenue: "92.2 billion"},
 	{ID: "2", Name: "Netflix", CEO: "Reed Hastings", Revenue: "20.2 billion"},
 	{ID: "3", Name: "Microsoft", CEO: "Satya Nadella", Revenue: "320 million"},
+	{ID: "4", Name: "Microsoft outro", CEO: "Satya Nadella asdf", Revenue: "30 million"},
 }
 
 func NewCompanyHandler(c *gin.Context) {
